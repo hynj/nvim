d3bee3412c7a2f9e3af0d0beddbb6ede74385f81
Nick Local <thenick112@gmail.com>
2024-04-02 20:26:17 +0000

    Hard mode

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,17 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.biome,
-        null_ls.builtins.diagnostics.biome
+        null_ls.builtins.formatting.biome.with({
+          args = {
+            'check',
+            '--apply-unsafe',
+            '--formatter-enabled=true',
+            '--organize-imports-enabled=true',
+            '--skip-errors',
+            '$FILENAME',
+          },
+        }),
+        null_ls.builtins.diagnostics.biome,
       },
     }
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
