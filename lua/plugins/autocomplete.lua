c9f23a31b526fccf917f02d10b21b2788195ed62
Nick Local <thenick112@gmail.com>
2024-09-09 11:04:12 +0000

    added find in file leader fd

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -50,6 +50,10 @@ return {
         }),
       })
 
+      cmp.setup.filetype("markdown", {
+          sources = {},
+      })
+
       -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>fd", function() builtin.live_grep({grep_open_files=true}) end, {})
   end,
   },
   {
